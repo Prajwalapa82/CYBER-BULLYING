e6db1e9cee8ec7b9a08a974289268989a6ff2531
Prajwala panigrahy <Prajwala panigrahy@DESKTOP-V9EEHKT>
2020-09-30 00:34:45 -0700

    my major project

---
+++ cyberbulling/WebContent/jsp/welcome.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Cyberbullying Detection based on
+Semantic-Enhanced Marginalized Denoising
+Auto-Encoder</title>
+</head>
+<body>
+<jsp:include page="/jsp/customer.jsp"></jsp:include>
+<jsp:include page="/jsp/menu.jsp"></jsp:include>
+</body>
+</html>
---
+++ cyberbulling/src/com/twitter/model/ProbabilityInfo.java
@@ -0,0 +1,63 @@
+package com.twitter.model;
+
+public class ProbabilityInfo {
+	
+	private int tweetId;
+	
+	private String catName;
+	
+	
+	public String getCatName() {
+		return catName;
+	}
+
+	public void setCatName(String catName) {
+		this.catName = catName;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getTotalWords() {
+		return totalWords;
+	}
+
+	public void setTotalWords(int totalWords) {
+		this.totalWords = totalWords;
+	}
+
+	
+	public double getProbability() {
+		return probability;
+	}
+
+	public void setProbability(double probability) {
+		this.probability = probability;
+	}
+
+
+	
+
+
+	public int getTweetId() {
+		return tweetId;
+	}
+
+	public void setTweetId(int tweetId) {
+		this.tweetId = tweetId;
+	}
+
+
+	private int count;
+	
+	private int totalWords;
+	
+	private double probability;
+	
+
+}
